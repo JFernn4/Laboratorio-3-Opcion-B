6f11a1bf75bbb6e8a30d4c3a6c16bf02248f699c
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-14 12:04:14 -0600

    Listas

--- Laboratorio 3 Opcion B/Program.cs
+++ Laboratorio 3 Opcion B/Program.cs
@@ -1,3 +1,5 @@
 ﻿using Laboratorio_3_Opcion_B;
 
 List<Platos> listaPlatos= new List<Platos>();
+List <Reservas> listaReservas= new List<Reservas>();
+List <Cliente> listaClientes= new List<Cliente>();
