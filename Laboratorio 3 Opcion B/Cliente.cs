9a6523d165bc872314f42fe3398d122590ba9dc0
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-14 11:57:46 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio 3 Opcion B/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3_Opcion_B
+{
+    internal class Cliente
+    {
+        public  string  Nombre { get; set; }
+        public string  Correo { get; set; }
+        public string Telefono { get; set; }
+
+        public Cliente(string nombre, string correo, string telefono)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Telefono = telefono;
+        }
+    }
+}
---
+++ Laboratorio 3 Opcion B/¨ClienteRegular.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3_Opcion_B
+{
+    internal class ClienteRegular : Cliente
+    {
+        public ClienteRegular(string nombre, string correo, string telefono) : base(nombre, correo, telefono)
+        {
+        }
+    }
+}
