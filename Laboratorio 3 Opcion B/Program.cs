7b0cd5d011fb4de4bcee27a51115e9a7e67e486a
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-14 12:06:17 -0600

    Menu

--- Laboratorio 3 Opcion B/Program.cs
+++ Laboratorio 3 Opcion B/Program.cs
@@ -3,3 +3,20 @@
 List<Platos> listaPlatos= new List<Platos>();
 List <Reservas> listaReservas= new List<Reservas>();
 List <Cliente> listaClientes= new List<Cliente>();
+bool menu = true;
+int opcion;
+while (menu)
+{
+    opcion=Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+
+    }
+}
+static void MostrarMenu()
+{
+    Console.WriteLine("Registrar cliente regular.");
+    Console.WriteLine("Registrar cliente VIP.");
+    Console.WriteLine("");
+    Console.WriteLine();
+}
